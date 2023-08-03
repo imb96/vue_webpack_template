a4ee073ae6913b2419b07bac8b139ffe61b6ee9d
김민재 <kimminje7267@naver.com>
2023-08-03 17:08:32 +0900

    Add router

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true
   },
   module: {
@@ -44,4 +45,7 @@ module.exports = {
       ]
     })
   ],
+  devServer: {
+    historyApiFallback: true
+  }
 }
